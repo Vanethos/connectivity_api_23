d80d71e7de6b3feecd415c2c704f09f3eb60e397
Gonçalo Palma <solid.goncalo@gmail.com>
2019-10-17 22:26:29 +0400

    fix min sdk version

--- android/src/main/java/io/flutter/plugins/connectivity/ConnectivityPlugin.java
+++ android/src/main/java/io/flutter/plugins/connectivity/ConnectivityPlugin.java
@@ -15,6 +15,10 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+
+import com.androidstudy.networkmanager.Monitor;
+import com.androidstudy.networkmanager.Tovuti;
+
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.EventChannel.EventSink;
 import io.flutter.plugin.common.EventChannel.StreamHandler;
@@ -52,20 +56,23 @@ public class ConnectivityPlugin implements MethodCallHandler, StreamHandler {
   }
 
   @Override
-  public void onListen(Object arguments, EventSink events) {
-    receiver = createReceiver(events);
-    registrar
-        .context()
-        .registerReceiver(receiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+  public void onListen(Object arguments, final EventSink events) {
+    Tovuti.from(registrar.context()).monitor(new Monitor.ConnectivityListener(){
+      @Override
+      public void onConnectivityChanged(int connectionType, boolean isConnected, boolean isFast){
+        events.success(checkNetworkType());
+      }
+    });
   }
 
   @Override
   public void onCancel(Object arguments) {
-    registrar.context().unregisterReceiver(receiver);
-    receiver = null;
+    Tovuti.from(registrar.context()).stop();
   }
 
   private String getNetworkType(ConnectivityManager manager) {
+    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
       Network network = manager.getActiveNetwork();
       NetworkCapabilities capabilities = manager.getNetworkCapabilities(network);
       if (capabilities == null) {
@@ -78,7 +85,31 @@ public class ConnectivityPlugin implements MethodCallHandler, StreamHandler {
       if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
         return "mobile";
       }
-      return null;
+    }
+
+    return getNetworkTypeLegacy(manager);
+  }
+
+  @SuppressWarnings("deprecation")
+  private String getNetworkTypeLegacy(ConnectivityManager manager) {
+    // handle type for Android versions less than Android 9
+    NetworkInfo info = manager.getActiveNetworkInfo();
+    if (info == null || !info.isConnected()) {
+      return "none";
+    }
+    int type = info.getType();
+    switch (type) {
+      case ConnectivityManager.TYPE_ETHERNET:
+      case ConnectivityManager.TYPE_WIFI:
+      case ConnectivityManager.TYPE_WIMAX:
+        return "wifi";
+      case ConnectivityManager.TYPE_MOBILE:
+      case ConnectivityManager.TYPE_MOBILE_DUN:
+      case ConnectivityManager.TYPE_MOBILE_HIPRI:
+        return "mobile";
+      default:
+        return "none";
+    }
   }
 
   @Override
@@ -151,14 +182,5 @@ public class ConnectivityPlugin implements MethodCallHandler, StreamHandler {
               (i_ip & 0xff), (i_ip >> 8 & 0xff), (i_ip >> 16 & 0xff), (i_ip >> 24 & 0xff));
 
     result.success(ip);
-  }
-
-  private BroadcastReceiver createReceiver(final EventSink events) {
-    return new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        events.success(checkNetworkType());
-      }
-    };
   }
 }
